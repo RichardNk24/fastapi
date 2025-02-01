f3b7c1a79abdbd2c0fa2ad3cb7d50dbba84c3706
Richard Nkulu <richardsobelnkulu@gmail.com>
2025-02-01 13:35:13 +0200

    add models.py with initial user structure

---
+++ models.py
@@ -0,0 +1,21 @@
+from uuid import UUID, uuid4
+from pydantic import BaseModel
+from typing import Optional, List
+from enum import Enum
+
+class Gender(str, Enum):
+    male = "male"
+    female = "female"
+    student = "student"
+    
+class Role(str, Enum):
+    admin = "admin"
+    user = "user"
+    
+class User(BaseModel):
+    id: Optional[UUID] = uuid4
+    first_name: str
+    last_name: str
+    middle_name: Optional[str]
+    gender: Gender
+    roles: List[Role]

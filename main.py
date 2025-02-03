3707e86869a24164096db182c90c5eb8ffefbdac
Richard Nkulu <richardsobelnkulu@gmail.com>
2025-02-03 13:51:32 +0200

    add db to main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from typing import List
+from uuid import uuid4
 from fastapi import FastAPI
+
+from models import User, Gender, Role
 
 app = FastAPI()
 
+db: List[User] = [
+    User(id=uuid4(), first_name="Luana", last_name="Channing", gender=Gender.female, roles=[Role.student] )
+]
 @app.get('/')
 async def root():
     return {"Hello": "Rich World"}
